b7b37293ec85f3d678e6e643f53db42dedd853f4
Leonardo Pequeño Moreno <A01178029@tec.mx>
2024-05-25 11:27:55 -0600

    flip fix y power ups spawn

---
+++ Template/Assets/Scripts/DisappearOnTouch.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DisappearOnTouch : MonoBehaviour
+{
+    // private SpecialAbility specialAbility;
+
+    // private void Start()
+    // {
+    //     // Try to get the SpecialAbility script attached to the same GameObject
+    //     specialAbility = GetComponent<SpecialAbility>();
+    // }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))  // Ensure the colliding object is the player
+        {
+            // Check if this GameObject's tag is "Shooter"
+            if (gameObject.tag == "PowerDown")
+            {
+                Debug.Log("Special Ability Activated!");
+                // specialAbility.Activate();  // Activate the Special Ability
+                // specialAbility.enabled = true; // Enable the script if needed
+            }
+
+            Destroy(gameObject);  // Destroy the object
+        }
+    }
+}
